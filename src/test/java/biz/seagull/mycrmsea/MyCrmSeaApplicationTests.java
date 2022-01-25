54ea1de3ad9f726a851cb06084f245d40a287c32
AlexNik13 <4pnav21@gmail.com>
2022-01-25 10:00:42 +0200

    Initial commit

---
+++ src/test/java/biz/seagull/mycrmsea/MyCrmSeaApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.seagull.mycrmsea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCrmSeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
